a1eb62f4067a8800b75add20f861d8d9c22b09d3
John M. Chambers <jmc@stat.stanford.edu>
2016-03-31 11:27:54 -0700

    Julia Examples package

---
+++ R/loadActions.R
@@ -0,0 +1,8 @@
+.loadSVD <- function(ns) {
+    genr <- setJuliaClass("SVD", where = ns)
+    assign("SVD", genr, envir = ns)
+    genr <- setRefClass("SvdJTimed", contains = "SVD",
+             fields =c(time = "numeric"), where = ns)
+    assign("SvdJTimed", genr, envir = ns)
+    assign("svdJ", JuliaFunction("svdfact")
+}
